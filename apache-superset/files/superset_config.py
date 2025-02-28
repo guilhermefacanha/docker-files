0bf4ed3f1f2c121e23be47b2655cd3b5a58e5880
guilhermelimafacanha <guilhermelimafacanha@localhost>
2025-02-28 09:53:31 -0800

    update apache superset docker files

---
+++ apache-superset/files/superset_config.py
@@ -0,0 +1,8 @@
+# Turning off i18n by default as translation in most languages are
+# incomplete and not well maintained.
+BABEL_DEFAULT_LOCALE = "pt_BR"
+
+LANGUAGES = {
+ 'en': {'flag': 'us', 'name': 'English'},
+ 'pt_BR': {'flag': 'br', 'name': 'Português'},
+}
